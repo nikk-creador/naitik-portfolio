0c6a01e4dc8a938e5c85a7ad3e3b06829aaac36e
Naitik Bhavsar <nbnik17@gmail.com>
2026-02-23 22:41:00 +0530

    APP file update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
-import "./App.css";
+﻿import React from "react";
+import { Layout } from "antd";
+import { AnimatePresence } from "framer-motion";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import styled from "styled-components";
+import { NAV_SECTIONS } from "./data/Home";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import FloatingIconBackdrop from "./components/layout/FloatingIconBackdrop";
+const Home = React.lazy(() => import("./components/sections/Home"));
+const BlogDetail = React.lazy(() =>
+  import("./components/sections/Blog").then((module) => ({
+    default: module.BlogDetail,
+  })),
+);
 
-function App(): JSX.Element {
+const { Content } = Layout;
+
+const StyledLayout = styled(Layout)`
+  min-height: 100vh;
+  width: 100%;
+  background: transparent;
+  position: relative;
+`;
+
+const StyledContent = styled(Content)`
+  flex: 1;
+  background: transparent;
+  padding-top: 0;
+  position: relative;
+  z-index: 1;
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <h1>Naitik Bhavsar - Portfolio</h1>
-      <p>React + TypeScript + Vite Setup</p>
-    </div>
+    <BrowserRouter
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+    >
+      <StyledLayout>
+        <FloatingIconBackdrop />
+        <Navbar sections={NAV_SECTIONS} />
+        <StyledContent>
+          <React.Suspense fallback={null}>
+            <AnimatePresence mode="wait">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blog/:slug" element={<BlogDetail />} />
+              </Routes>
+            </AnimatePresence>
+          </React.Suspense>
+        </StyledContent>
+        <Footer />
+      </StyledLayout>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
